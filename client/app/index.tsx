93014a2ebde63e3143623e91f2eefe95e9623ab8
JayZenith <jayzenith248@gmail.com>
2024-10-07 14:14:45 -0700

    added header of app with some styling

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,19 +1,39 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { SafeAreaView, ScrollView } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { View, Text } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <div>
-      <View><Text>hello</Text></View>
+      <SafeAreaView>
+        <ScrollView style={styles.mainScrollContainer}>
+          <View style={styles.mainInnterContainer}>
+            <Text style={styles.title}>Welcome to Speech to Text App</Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </div>
   );
 }
 
 const styles = StyleSheet.create({
+  mainScrollContainer:{
+    height: "100%",
+    width: "100%",
+    padding: 20,
+  },
+
+  mainInnterContainer: {
+    height: "100%",
+    alignItems: "center", 
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 35,
+    padding: 5,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
+  },
   
 });
